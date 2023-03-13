094e39e8d5bf05c185bfc9504e9860b5bbd8e35a
unknown <balasdeep@gmail.com>
2023-03-13 22:42:01 +0530

    cors added

--- app.js
+++ app.js
@@ -1,40 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const cors = require("cors");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
-const {MONGOURI} = require('./config/key');
+const { MONGOURI } = require("./config/key");
 
- 
-mongoose.connect(MONGOURI,{
-    useNewUrlParser:true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-})
-mongoose.connection.on('connected',() => {
-    console.log('connected to mongo yehh');
-})
-mongoose.connection.on('error',(err)=>{
-    console.log('err connecting',err)
-})
+mongoose.connect(MONGOURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongo yehh");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("err connecting", err);
+});
 
-require('./models/userModel');
-require('./models/postModel');
+require("./models/userModel");
+require("./models/postModel");
 
-app.use(express.json());   // for read the json data
-app.use(require('./routes/auth'));
-app.use(require('./routes/post'));
-app.use(require('./routes/user'));
+app.use(cors());
+app.use(express.json()); // for read the json data
+app.use(require("./routes/auth"));
+app.use(require("./routes/post"));
+app.use(require("./routes/user"));
 
-if(process.env.NODE_ENV == "production"){
-    app.use(express.static('client/build'));
-    const path = require('path');
-    app.get("*",(req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
-    })
+app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Credential", "true");
+  app.send("API is running");
+});
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
-
-
 app.listen(PORT, () => {
-    console.log('server is running on', PORT);
-})
+  console.log("server is running on", PORT);
+});
